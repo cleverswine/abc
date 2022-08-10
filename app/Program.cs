59a019d3eefcd4db8e87edf3b9472ad3f62b2342
cleverswine <cleverswine@gmail.com>
2022-08-10 13:41:39 -0700

    add latest etsy items

--- app/Program.cs
+++ app/Program.cs
@@ -1,10 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-var dataPath = "app/data";
+﻿if (args.Count() < 1)
+{
+    throw new ArgumentException("specify a base path on the command line please.");
+}
+
+var basePath = args[0];
+var dataFileName = Path.Combine(basePath, "src/assets/data/rss.json");
+var imgPath = Path.Combine(basePath, "public/images");
+Console.WriteLine($"dataPath={dataFileName} / imgPath={imgPath}");
 
 var rss = new Rss(new HttpClient());
-var items = await rss.Get(new Uri("https://www.etsy.com/shop/auntieboocrafts/rss"));
-await rss.SaveAll(items, dataPath);
-foreach (var item in items)
+var remoteItems = await rss.Get(new Uri("https://www.etsy.com/shop/auntieboocrafts/rss"));
+var localItems = (await rss.Get(dataFileName)).ToList();
+
+foreach (var remoteItem in remoteItems)
 {
-    await rss.SaveImage(item, dataPath);
+    if (!localItems.Exists(x => x.ImageUrl == remoteItem.ImageUrl))
+    {
+        Console.WriteLine("adding new item: ");
+        Console.WriteLine(remoteItem.Title);
+        localItems.Add(remoteItem);
+    }
+
+    await rss.SaveImage(remoteItem, imgPath);
 }
+
+await rss.SaveAll(localItems.OrderByDescending(x => x.PubDate), dataFileName);
--- app/Rss.cs
+++ app/Rss.cs
@@ -19,28 +19,19 @@ public class Rss
         _httpClient = httpClient;
     }
 
+    public async Task<IEnumerable<RssItem>> Get(string path)
+    {
+        if (!File.Exists(path)) return new List<RssItem>();
+        return JsonSerializer.Deserialize<IEnumerable<RssItem>>(await File.ReadAllBytesAsync(path));
+    }
+
     public async Task<IEnumerable<RssItem>> Get(Uri url)
     {
-        return Get(await _httpClient.GetStringAsync(url));
-    }
-
-    public async Task SaveImage(RssItem item, string path)
-    {
-        if(File.Exists(item.ImageName)) return;
-        var img = await _httpClient.GetByteArrayAsync(item.ImageUrl);
-        File.WriteAllBytes($"{path}/{item.ImageName}", img);
-    }
-
-    public async Task SaveAll(IEnumerable<RssItem> items, string path)
-    {
-        await File.WriteAllTextAsync($"{path}/rss.json", JsonSerializer.Serialize(items));
-    }
-
-    public IEnumerable<RssItem> Get(string s)
-    {
+        var s = await _httpClient.GetStringAsync(url);
         var xml = new XmlDocument();
         xml.LoadXml(s);
 
+        var result = new List<RssItem>();
         var items = xml.SelectNodes("rss/channel/item");
         foreach (XmlNode rssNode in items)
         {
@@ -55,14 +46,32 @@ public class Rss
             var imgEnd = description.IndexOf(imgEndString, imgStart);
             var img = description.Substring(imgStart, imgEnd - imgStart);
 
-            yield return new RssItem
+            result.Add(new RssItem
             {
                 Title = title.Replace("by AuntiBooCrafts", ""),
                 ImageUrl = img,
                 ImageName = img.Split('/').Last(),
                 SourceLink = link,
                 PubDate = DateTime.Parse(pubDate)
-            };
+            });
         }
+        return result;
+    }
+
+    public async Task SaveImage(RssItem item, string path)
+    {
+        if (File.Exists(item.ImageName)) return;
+        var img = await _httpClient.GetByteArrayAsync(item.ImageUrl);
+        File.WriteAllBytes($"{path}/{item.ImageName}", img);
+    }
+
+    public async Task SaveAll(IEnumerable<RssItem> items, string fileName)
+    {
+        if (File.Exists(fileName))
+        {
+            if (File.Exists(fileName + ".bk")) File.Delete(fileName + ".bk");
+            File.Copy(fileName, fileName + ".bk");
+        }
+        await File.WriteAllTextAsync($"{fileName}", JsonSerializer.Serialize(items));
     }
 }
